6181637618cb747e48cb31b455c84f9a6459aea4
rafaelterssetti <rafaelterssetti@gmail.com>
2025-12-30 20:41:48 -0300

    atualizações

---
+++ prime.py
@@ -0,0 +1,25 @@
+import math
+
+def is_prime(n):
+    """verifica se um número é primo"""
+
+    if n <= 1:
+        return False
+    for i in range(2,int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+
+    """contém toda a lógica principal"""
+
+    for i in range(100):
+        if is_prime(i):
+            print (i, end=' ')
+    print()
+
+
+if __name__ == '__main__':
+    main()
